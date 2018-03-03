564289521dc6fb28d5af4a91cca546e6fbd1bfdc
Vijay Krishan Pandey <v.k.pandeycool98@gmail.com>
2018-03-04 03:14:16 +0530

    Add files via upload

---
+++ background.js
@@ -0,0 +1,22 @@
+
+chrome.browserAction.onClicked.addListener(function(tab){
+chrome.windows.getAll({populate:true},function(windows){
+	windows.forEach(function(window){
+		window.tabs.forEach(function(tab){
+			var str=tab.url;
+			if(str.includes("facebook")||str.includes("twitter")||str.includes("instagram")||str.includes("youtube"))
+			{
+				chrome.tabs.remove(tab.id);
+			}	
+		});
+	});
+});
+
+var newURL = "https://stackoverflow.com/";
+chrome.tabs.create({ url: newURL });
+newURL="https://codechef.com/";
+chrome.tabs.create({url:newURL});
+newURL="https://www.geeksforgeeks.org/"
+chrome.tabs.create({url:newURL});
+
+});
