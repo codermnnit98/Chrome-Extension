527f44a34da4da25a94444292c17c16588ed42a6
Ridhima Thakur <34443495+ridhima-thakur@users.noreply.github.com>
2020-10-01 00:49:27 +0530

    Update background.js

--- background.js
+++ background.js
@@ -4,7 +4,7 @@ chrome.windows.getAll({populate:true},function(windows){
 	windows.forEach(function(window){
 		window.tabs.forEach(function(tab){
 			var str=tab.url;
-			if(str.includes("facebook")||str.includes("twitter")||str.includes("instagram")||str.includes("youtube"))
+			if(str.includes("facebook")||str.includes("twitter")||str.includes("instagram")||str.includes("youtube")||str.includes("whatsapp")||str.include("netflix"))
 			{
 				chrome.tabs.remove(tab.id);
 			}	
@@ -18,5 +18,10 @@ newURL="https://codechef.com/";
 chrome.tabs.create({url:newURL});
 newURL="https://www.geeksforgeeks.org/"
 chrome.tabs.create({url:newURL});
+newURL="https://www.codeforces.com/"
+chrome.tabs.create({url:newURL});
+newURL="https://www.atcoder.jp/"
+chrome.tabs.create({url:newURL});
+	
 
 });
